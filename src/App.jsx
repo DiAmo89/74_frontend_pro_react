69d3d8085dc80ea76c5f5daa745f09bf23951628
DiMa <dima2025dim89@gmail.com>
2026-01-06 17:39:20 +0100

    Add Feedback component with Like, Dislike and Reset button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button/Button";
+// import Button from "./components/Button/Button";
+import Feedback from "./components/Feedback/Feedback";
 
 function App() {
-const [count,setCount] = useState(0);
-
+  // const [count,setCount] = useState(0);
 
   return (
-    <div className="app">
-      React Introduce
-      <Button name="plus" btnClick={()=>setCount((prev)=>prev+1)}/>
-      <Button name="minus" btnClick={()=>setCount((prev)=>prev-1)}/>
-      <div>{count}</div>
+    <div className="feedback-container">
+      <h1>Feedback</h1>
+      <Feedback />
     </div>
   );
+
+  // <div className="app">
+  //   React Introduce
+  //   <Button name="plus" btnClick={()=>setCount((prev)=>prev+1)}/>
+  //   <Button name="minus" btnClick={()=>setCount((prev)=>prev-1)}/>
+  //   <div>{count}</div>
+  // </div>
 }
 
 export default App;
